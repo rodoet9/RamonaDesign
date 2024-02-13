92e52179fc7d5037e8155082eba65b041f8e36f0
TeodorBadarcea <teodor.badarcea@bearingpoint.com>
2024-02-13 23:51:01 +0200

    changed imgs path

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
@@ -8,7 +7,7 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 	pauseOnHover = true;
 
-	imagesFolderPath = '../../assets/homeCarousel/';
+	imagesFolderPath = '/assets/homeCarousel/';
 
 	imageNames = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg', 'img7.jpg'];
 
