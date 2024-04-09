5eca6bc254cd5419c8ff707f074abe980d7e84b7
Teodor Badarcea <teodor.badarcea@bearingpoint.com>
2024-04-09 17:45:16 +0300

    added new sections on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 	pauseOnHover = true;
+	quote: string = 'An empty room is a story waiting to happen, and we are the authors.';
+	typedText: string = '';
+	typingStarted: boolean = false;
+
+	constructor() {}
+
+	ngOnInit() {
+		this.typeWriter();
+		this.typingStarted = true;
+	}
 
 	imagesFolderPath = '/assets/homeCarousel/';
 
-	imageNames = ['img1.jpg', 'img2.jpg', 'img3.jpg', 'img4.jpg', 'img5.jpg', 'img6.jpg', 'img7.jpg'];
+	imageNames = ['img1.jpeg', 'img2.jpeg', 'img3.jpeg', 'img4.jpeg', 'img5.jpeg', 'img6.jpeg', 'img7.jpeg'];
 
 	getImagePath(imageName: string): string {
 		const imagePath = this.imagesFolderPath + imageName;
 		return imagePath;
+	}
+
+	typeWriter() {
+		let i = 0;
+		const speed = 100;
+		const typingInterval = setInterval(() => {
+			if (i < this.quote.length) {
+				this.typedText += this.quote.charAt(i);
+				i++;
+			} else {
+				clearInterval(typingInterval);
+			}
+		}, speed);
 	}
 }
