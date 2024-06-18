4a0f303be10f413529492ef74ff0e60452915b8e
Teodor Badarcea <teodor.badarcea@bearingpoint.com>
2024-06-18 15:26:09 +0300

    implement vercel analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { inject } from '@vercel/analytics';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'rdesign';
+	title = 'rdesign';
 }
+
+inject();
